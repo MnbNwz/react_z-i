33d9a2088aea846ca6d57bc13ec12d36f57bf835
Muneeb Nawaz <muneeb.nawaz@ts.com>
2023-11-10 18:02:31 +0500

    commiting

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -2,8 +2,19 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import "./carousel.css";
+import image1 from "../../images/trucks/1.jpg";
+import image2 from "../../images/trucks/2.jpeg";
+import image3 from "../../images/trucks/3.jpg";
+import image4 from "../../images/trucks/4.jpg";
+import image5 from "../../images/trucks/5.jpeg";
+import image6 from "../../images/trucks/6.jpeg";
 
 const Carousel = () => {
+  const ImageForCarousel = (url) => (
+    <div>
+      <img alt="Truck" src={url} />
+    </div>
+  );
   let settings = {
     dots: true,
     infinite: true,
@@ -13,27 +24,35 @@ const Carousel = () => {
   };
   return (
     <Slider {...settings}>
-      <div>
-        <img
-          className="carousel_div"
-          src={require("../../images/trucks/1.jpg")}
-        />
+      {ImageForCarousel(image1)}
+      {ImageForCarousel(image2)}
+      {ImageForCarousel(image3)}
+      {ImageForCarousel(image4)}
+      {ImageForCarousel(image5)}
+      {ImageForCarousel(image6)}
+      {/* {ImageForCarousel("../../images/trucks/3.jpg")}
+      {ImageForCarousel("../../images/trucks/4.jpg")}
+      {ImageForCarousel("../../images/trucks/5.jpeg")}
+      {ImageForCarousel("../../images/trucks/6.jpeg")} */}
+
+      {/* <div>
+        <img alt="Truck" src={require("../../images/trucks/1.jpg")} />
       </div>
       <div>
-        <img src={require("../../images/trucks/2.jpeg")} />
+        <img alt="Truck" src={require("../../images/trucks/2.jpeg")} />
       </div>
       <div>
-        <img src={require("../../images/trucks/3.jpg")} />
+        <img alt="Truck" src={require("../../images/trucks/3.jpg")} />
       </div>
       <div>
-        <img src={require("../../images/trucks/4.jpg")} />
+        <img alt="Truck" src={require("../../images/trucks/4.jpg")} />
       </div>
       <div>
-        <img src={require("../../images/trucks/5.jpeg")} />
+        <img alt="Truck" src={require("../../images/trucks/5.jpeg")} />
       </div>
       <div>
-        <img src={require("../../images/trucks/6.jpeg")} />
-      </div>
+        <img alt="Truck" src={require("../../images/trucks/6.jpeg")} />
+      </div> */}
     </Slider>
   );
 };
